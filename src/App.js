d9e9a7d5c264ba4d558788f3294307fdf550f244
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-07-03 20:38:17 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div></div>
+)
 
 function App() {
   return (
