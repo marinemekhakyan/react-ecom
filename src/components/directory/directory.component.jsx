9ae0decbd99b4a4c9d714dc72f82ce44791fa4f6
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-07-05 16:42:25 -0700

    routing individual components with withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,9 +48,16 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-                    ))}
+                    // this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+                    //     <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
+                    // ))
+
+                    //Instead of the long code above, we can spread the key value pairs in "otherSectionProps" like below
+                    
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                      <MenuItem key={id} {...otherSectionProps} />
+                    ))
+                }
             </div>
         )
     }
