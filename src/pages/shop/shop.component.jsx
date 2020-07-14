4b8968926b4904912ee879aa9e664f971fa0f765
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-07-13 21:27:51 -0700

    adding shop page, collection preview and collection item components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview-component';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 // import { render } from '@testing-library/react';
 
 class ShopPage extends React.Component {
