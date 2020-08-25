859c81bbbee46f4049ea7c1dcdb9992c07913765
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-08-24 23:00:09 -0700

    adding cart icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,29 +4,34 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import { connect } from 'react-redux';
 //connect is a higher order component that let's us modify our component to have access to things related to redux
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
     <div className='header'>
-        <Link to='/'>
-            <Logo className='logo'/>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
         </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='/contact'>
-                {
-                    currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className='option' to='/signin'>SIGN IN</Link>
-                }
-            </Link>
-        </div>
-
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
+      </div>
     </div>
-);
+  );
 
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser
